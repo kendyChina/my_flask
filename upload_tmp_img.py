9704372fd370cdf58ea983b41e94d4260ffa4785
kendyChina <632655036@qq.com>
2019-07-26 17:47:43 +0800

    init

--- upload_tmp_img.py
+++ upload_tmp_img.py
@@ -18,26 +18,19 @@ def upload_tmp_img():
 			data = {"media": img}
 			resp = requests.post(url, data=data)
 
-			params = {
-				"access_token": access_token,
-				"type": "image"
-			}
+			params = {"access_token": access_token,"type": "image"}
 			with open(img, "rb") as f:
 				payload = f.read()
 			content_type = "multipart/form-data"
-			headers = {
-				"Content-Type": content_type
-			}
-			data = {
-				"data": (img, payload, content_type)
-			}
+			headers = {"Content-Type": content_type}
+			data = {"data": (img, payload, content_type)}
 			resp = requests.post(url, files=data, params=params, headers=headers)
 
 			print(resp.text)
 			media_id = json.loads(resp.text)["media_id"]
 			xm_id[xm] = media_id
 
-		c.execute("INSERT INTO xm_media VALUES (?, ?)", (xm, media_id))
+			c.execute("INSERT INTO xm_media VALUES (?, ?)", (xm, media_id))
 	print(xm_id)
 
 if __name__ == "__main__":
