bec44e0a6bcc8a48f2f873483ddf9e4710229f3a
kendyChina <632655036@qq.com>
2019-07-30 19:03:33 +0800

    init

--- jf.py
+++ jf.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-import os
+import os, time, datetime
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -26,27 +26,11 @@ jf["首充≥50"] = jf["首次充值金额"].map(lambda x: "是" if x >= 50 else
 xm_set = set(jf["项目"])
 xm_img = {}
 
-# 绘制项目的转化表，并把{xm:img}对应关系作为xm_img返回
-def get_img(xm):
-	# 项目可以用groupby来划分
-	xm = jf[jf["项目"] == xm]
-
-	groupbyrq = xm.groupby("外部下单日期") # 以外部下单日期为group
-	# xmtb = 项目通报
-	xmtb = groupbyrq["外部订单号", "入网时间", "首充≥50"].count() # 计算订单量、激活量、首充量
-	xmtb.loc["汇总"] = xmtb.apply(lambda x: x.sum()) # 增加汇总行，为订单量、激活量、首充量的总和
-
-	# 计算几乎率、充值率、综转率，并以xx.xx%格式输出
-	xmtb["激活率"] = (xmtb["入网时间"] / xmtb["外部订单号"]).apply(lambda x: format(x, ".2%"))
-	xmtb["充值率"] = (xmtb["首充≥50"] / xmtb["入网时间"]).apply(lambda x: format(x, ".2%"))
-	xmtb["综转率"] = (xmtb["首充≥50"] / xmtb["外部订单号"]).apply(lambda x: format(x, ".2%"))
-
-	xmtb.rename(columns={"外部订单号": "订单量", "入网时间": "激活量"}, inplace=True) # 重命名表头
-	# xmtb.style.bar(subset=["激活率", "充值率", "综转率"], color="#d65f5f", width=100)
+def draw_jpg(xm, df):
 
 	# 隔行设置row颜色：灰白相间
 	rowcolor = []
-	for i in range(len(xmtb.index)):
+	for i in range(len(df.index)):
 		if i % 2 == 0:
 			rowcolor.append("#DCDCDC") # 浅灰
 		else:
@@ -54,22 +38,19 @@ def get_img(xm):
 
 	# 隔行设置cell颜色：灰白相间
 	cellcolor = []
-	for i in range(len(xmtb.index)):
+	for i in range(len(df.index)):
 		if i % 2 == 0:
 			color = "#DCDCDC" # 浅灰
 		else:
 			color = "white"
-		# temp = []
-		# for j in range(len(xmtb.columns)):
-		# 	temp.append(color)
-		# cellcolor.append(temp)
-		cellcolor.append([color for x in range(len(xmtb.columns))])
+		cellcolor.append([color for x in range(len(df.columns))])
 
+	# 设置表格样式
 	plt.table(
-		cellText=xmtb.values,
-		colLabels=xmtb.columns,
-		colWidths=[0.13]*len(xmtb.columns),
-		rowLabels=xmtb.index,
+		cellText=df.values,
+		colLabels=df.columns,
+		colWidths=[0.15]*len(df.columns),
+		rowLabels=df.index,
 		loc="best",
 		cellLoc="center",
 		colLoc="center",
@@ -84,24 +65,97 @@ def get_img(xm):
 
 	plt.axis("off")
 
-	plt.title("%s订单情况" % xm) # 设置标题
+	plt.title("%s订单情况(%s日)" % (xm, time.strftime("%m-%d", time.localtime(time.time())))) # 设置标题
 
-	target = os.path.join(target_path, "%s.jpg" % xm) # 保存为jpg
-	plt.savefig(target, dpi=150)
+	plt.gcf().set_size_inches(10.0/1.5, 15.0/1.5)
+	target = os.path.join(target_path, r"%s.jpg" % xm) # 保存为jpg
+	plt.savefig(target, bbox_inches="tight", pad_inches=0.5)
 	plt.close()
 
 	xm_img[xm] = target
 
-def run_jf(self):
+def get_two_month_dates():
+	# 获取本月的年月
+	this_year = datetime.datetime.now().year
+	this_month = datetime.datetime.now().month
+
+	# 获取本月的第一天和今天
+	this_first_day = datetime.date(this_year, this_month, 1)
+	this_today = time.strftime("%Y-%m-%d", time.localtime())
+
+	# 获取上月的最后一天
+	prev_last_day = this_first_day - datetime.timedelta(days=1)
+
+	# 通过上月的最后一天获取上月的天数
+	prev_periods = prev_last_day.day
+
+	# 获取近两个月的日期索引
+	this_dates = pd.date_range(start=this_first_day, end=this_today).strftime("%Y-%m-%d")
+	prev_dates = pd.date_range(end=prev_last_day, periods=prev_periods).strftime("%Y-%m-%d")
+
+	return (this_dates, prev_dates)
+
+def get_img(xm):
+
+	xmmx = jf[jf["项目"] == xm] # 项目明细
+
+	this_dates, prev_dates = get_two_month_dates()
+	colunms = ["订单量", "激活量", "激活率", "首充≥50", "充值率", "综转率", "当日激活", "当日产能"]
+	# prev_df = pd.DataFrame(data=None, index=prev_dates, columns=colunms)
+	# for date in prev_dates:
+	# 	date_df = xmmx["外部下单日期"] == date
+	# 	prev_df.loc[date, "订单量"] = xmmx[date_df]["外部订单号"].count()
+	# 	prev_df.loc[date, "激活量"] = xmmx[date_df]["入网时间"].count()
+	# 	prev_df.loc[date, "首充≥50"] = xmmx[date_df]["首充≥50"].count()
+	# 	prev_df.loc[date, "当日激活"] = xmmx[xmmx["激活日期"] == date]["激活日期"].count()
+	# 	prev_df.loc[date, "当日产能"] = xmmx[xmmx["首次充值时间"] == date]["首充≥50"].count()
+	# prev_df.loc["%s月汇总" % prev_dates[0][5:7]] = prev_df.apply(lambda x: x.sum())
+	# prev_df = pd.DataFrame(data=prev_df.tail(1))
+	# prev_df["激活率"] = (prev_df["激活量"] / prev_df["订单量"]).apply(lambda x: format(x, ".2%"))
+	# prev_df["充值率"] = (prev_df["首充≥50"] / prev_df["激活量"]).apply(lambda x: format(x, ".2%"))
+	# prev_df["综转率"] = (prev_df["首充≥50"] / prev_df["订单量"]).apply(lambda x: format(x, ".2%"))
+	# print(prev_df)
+
+	this_df = pd.DataFrame(data=None, index=this_dates, columns=colunms)
+	for date in this_dates:
+		date_df = xmmx["外部下单日期"] == date
+		this_df.loc[date, "订单量"] = xmmx[date_df]["外部订单号"].count()
+		this_df.loc[date, "激活量"] = xmmx[date_df]["入网时间"].count()
+		this_df.loc[date, "首充≥50"] = xmmx[date_df]["首充≥50"].count()
+		this_df.loc[date, "当日激活"] = xmmx[xmmx["激活日期"] == date]["激活日期"].count()
+		this_df.loc[date, "当日产能"] = xmmx[xmmx["首次充值日期"] == date]["首充≥50"].count()
+	this_df.loc["%s月汇总" % this_dates[0][5:7]] = this_df.apply(lambda x: x.sum())
+	try:
+		this_df["激活率"] = (this_df["激活量"] / this_df["订单量"]).apply(lambda x: format(x, ".2%"))
+		this_df["充值率"] = (this_df["首充≥50"] / this_df["激活量"]).apply(lambda x: format(x, ".2%"))
+		this_df["综转率"] = (this_df["首充≥50"] / this_df["订单量"]).apply(lambda x: format(x, ".2%"))
+	except:
+		this_df["激活率"] = format(0, ".2%")
+		this_df["充值率"] = format(0, ".2%")
+		this_df["综转率"] = format(0, ".2%")
+
+	return this_df
+
+def run_jf():
 	with sqlite3.connect(db) as conn:
 		c = conn.cursor()
 		c.execute("DELETE FROM xm_img;") # 删除所有xm_img内容，以便重新插入
 		for xm in xm_set:
-			get_img(xm)
+			draw_jpg(xm, get_img(xm))
 		for xm, img in xm_img.items():
 			c.execute("INSERT INTO xm_img VALUES (?, ?)", (xm, img))
 	return xm_img
 
+def test():
+	xm_time = {}
+	for xm in xm_set:
+		starttime = time.time()
+		draw_jpg(xm, get_img(xm))
+		pay_time = time.strftime("%M-%S", time.localtime(time.time() - starttime))
+		xm_time[xm] = pay_time
+		print("%s: %s" % (xm, pay_time))
+
 if __name__ == "__main__":
+	# test()
 	run_jf()
 	# 返回项目和图片的对应关系
